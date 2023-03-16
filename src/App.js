faea0cdc2d7171bf6f297f1b80eee36eff3b205c
shahzaibsaleem <shahzaibsaleem665@gmail.com>
2023-03-17 05:53:54 +1100

    To run this code simply install react dependencies and run it. Code is written in very basic react.js.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setContacts(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const [activeContact, setActiveContact] = useState(null);
+
+  const handleClick = (contact) => {
+    setActiveContact(contact);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contacts</h1>
+      <ul>
+        {contacts.map((contact) => (
+          <li key={contact.id}>
+            <button onClick={() => handleClick(contact)}>
+              {contact.name}
+            </button>
+          </li>
+        ))}
+
+      </ul> 
+      {activeContact && (
+        <div className="contact-info">
+          <h2>{activeContact.name}</h2>
+          <p>{activeContact.email}</p>
+          <p>{activeContact.phone}</p>
+          <p>{activeContact.website}</p>
+        </div>
+      )}
     </div>
   );
 }
